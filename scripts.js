75b7633fd50b23a7f565be33cf3b73396d951152
JuanBueno <121510804+JuanBueno-coder@users.noreply.github.com>
2023-11-03 20:27:32 +0100

    add js file

---
+++ scripts.js
@@ -0,0 +1,13 @@
+console.log('Hello world');
+/*Juego se compone de:
+seleccion del ordenador
+    seleccionar uno de los tres movimientos posibles
+seleccion del player
+ver quien gana
+dar ganador
+*/
+let computerMove = getComputerChoice;
+
+function getComputerChoice(){
+
+}
