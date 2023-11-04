6b3194e89a6e89b3e6499638673acd4de2097892
JuanBueno <121510804+JuanBueno-coder@users.noreply.github.com>
2023-11-04 14:02:02 +0100

    added scripts

--- scripts.js
+++ scripts.js
@@ -6,8 +6,28 @@ seleccion del player
 ver quien gana
 dar ganador
 */
-let computerMove = getComputerChoice;
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
 function getComputerChoice(){
-
+    let election = getRandomInt(3);
+    if ( election == 0){return 'paper'
+       console.log('Computer has select paper');
+    }
+    if(election == 1){return 'rock'
+       console.log('Computer has select rock');
+    }
+    if(election == 2){return 'scissors'
+       console.log('Computer has select scissors');
+    }
 }
+function playRound(playerSelection, computerSelection) {
+if(playerSelection==computerSelection){
+    return console.log("")
+}
+  }
+const playerSelection ="rock";
+const computerSelection =getComputerChoice();
+console.log(computerSelection);
